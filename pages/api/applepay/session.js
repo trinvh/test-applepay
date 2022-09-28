4ea10b48f4ccd021f763107112a70fd535c72f53
Tri Nguyen <tri@platformfactory.io>
2022-09-28 22:24:13 +0700

    readded cert

--- pages/api/applepay/session.js
+++ pages/api/applepay/session.js
@@ -2,7 +2,7 @@ import * as fs from "node:fs";
 import * as path from 'node:path';
 import * as request from 'request'
 
-const certPath = path.join(process.cwd(), 'merchant_id.pem')
+const certPath = path.join(process.cwd(), 'certs/merchant_id.pem')
 const cert = fs.readFileSync(certPath, "utf8");
 
 export default function handler(req, res) {
